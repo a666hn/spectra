90d785c2f80434def29bbabd96c5d6558df6d4e0
Adi Hermawan <adidot78@gmail.com>
2020-10-07 13:58:48 +0700

    test import proto

--- core/protoc.sh
+++ core/protoc.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 protoc --proto_path=${pwd}:. --micro_out=. --go_out=. proto/account/*.proto
+protoc --proto_path=${pwd}:. --micro_out=. --go_out=. proto/customer/*.proto
+protoc --proto_path=${pwd}:. --micro_out=. --go_out=. proto/roles/*.proto
